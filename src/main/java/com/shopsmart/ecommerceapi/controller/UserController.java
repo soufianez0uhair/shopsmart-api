3f9f3891b09ae6ef56aa1e7235bc1927c9d25f34
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-25 22:00:06 +0100

    Add customer registration endpoint

--- src/main/java/com/shopsmart/ecommerceapi/controller/UserController.java
+++ src/main/java/com/shopsmart/ecommerceapi/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.shopsmart.ecommerceapi.controller;
 
+import com.shopsmart.ecommerceapi.model.User;
+import com.shopsmart.ecommerceapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    private ResponseEntity<String> registerCustomer(@RequestBody User user) {
+        return new ResponseEntity<String>(
+                userService.registerCustomer(user),
+                HttpStatus.CREATED
+        );
+    }
 }
--- src/test/java/com/shopsmart/ecommerceapi/controller/UserController.java
+++
@@ -1,2 +0,0 @@
-package com.shopsmart.ecommerceapi.controller;public class UserController {
-}
