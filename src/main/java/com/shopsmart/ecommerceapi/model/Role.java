69921cb09cb8a75b02835e173c9c75cbe40e5b3a
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-25 19:46:59 +0100

    Create Role model

---
+++ src/main/java/com/shopsmart/ecommerceapi/model/Role.java
@@ -0,0 +1,19 @@
+package com.shopsmart.ecommerceapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    private String name;
+}
