94b7788461f5e13666a8810e28c34a2e18365dd8
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-25 22:02:14 +0100

    Add customer registration endpoint unit test

--- src/test/java/com/shopsmart/ecommerceapi/controller/UserControllerTest.java
+++ src/test/java/com/shopsmart/ecommerceapi/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,16 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(UserController.class)
+@WebMvcTest
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private UserService userService;
 
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void givenValidUser_whenRegisterCustomer_thenReturn201AndToken() throws Exception {
