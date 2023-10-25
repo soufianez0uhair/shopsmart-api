cacee909115bfaec84df508ba63530179af19e94
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-25 21:41:04 +0100

    Create UserService and add registerCustomer

---
+++ src/main/java/com/shopsmart/ecommerceapi/service/UserService.java
@@ -0,0 +1,12 @@
+package com.shopsmart.ecommerceapi.service;
+
+import com.shopsmart.ecommerceapi.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String registerCustomer(User user) {
+        return "";
+    }
+}
