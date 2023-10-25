94a2a232ba10f765240022a8b9a31271b5e9355a
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-25 17:12:38 +0100

    Create JWTUtils and add extract username method

---
+++ src/main/java/com/shopsmart/ecommerceapi/util/JWTUtils.java
@@ -0,0 +1,40 @@
+package com.shopsmart.ecommerceapi.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JWTUtils {
+
+    private final static String SECRET_KEY = "d68f18b49b2e88f68a7c3d6f028efecaa160a22ca7130ba084b0789ddad7a3e1";
+
+    private static Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public static String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private static Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public static <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+}
