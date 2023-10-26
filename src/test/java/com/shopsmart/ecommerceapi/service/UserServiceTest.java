31e0a39e82276096987a9815fcdd61f810ae04a2
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-26 10:50:50 +0100

    pass the successful customer registration test

--- src/test/java/com/shopsmart/ecommerceapi/service/UserServiceTest.java
+++ src/test/java/com/shopsmart/ecommerceapi/service/UserServiceTest.java
@@ -1,22 +1,71 @@
 package com.shopsmart.ecommerceapi.service;
 
+import com.shopsmart.ecommerceapi.model.User;
 import com.shopsmart.ecommerceapi.repository.UserRepository;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import com.shopsmart.ecommerceapi.util.JWTUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
 
-    private UserService userService;
+    @Mock
+    private JWTUtils jwtUtils;
 
-    @BeforeAll
-    public void setUp() throws Exception {
-        userService = new UserService(userRepository);
+    @InjectMocks
+    private UserService underTest;
+
+    @Test
+    public void givenNewCustomer_whenRegisterCustomer_thenReturnTokenWithCustomerRole() {
+        // Given
+        User user = User
+                .builder()
+                .firstName("test")
+                .lastName("test")
+                .email("test@test.com")
+                .phoneNumber("+212600000000")
+                .password("test@123")
+                .build();
+        given(userRepository.findByEmail(user.getEmail())).willReturn(Optional.empty());
+        given(jwtUtils.generateToken(user)).willReturn("someReturnedToken");
+
+        // When
+        String returnedValue = underTest.registerCustomer(user);
+        // Then
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        verify(userRepository).findByEmail(stringCaptor.capture());
+        assertEquals(user.getEmail(), stringCaptor.getValue());
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userCaptor.capture());
+        User passedUserToSave = userCaptor.getValue();
+        assertEquals("test", passedUserToSave.getFirstName());
+        assertEquals("test", passedUserToSave.getLastName());
+        assertEquals("test@test.com", passedUserToSave.getEmail());
+        assertEquals("+212600000000", passedUserToSave.getPhoneNumber());
+        assertEquals("test@123", passedUserToSave.getPassword());
+        assertEquals("someReturnedToken", returnedValue);
+        verify(jwtUtils).generateToken(userCaptor.capture());
+        User passedUserToJWT = userCaptor.getValue();
+        assertEquals("test", passedUserToJWT.getFirstName());
+        assertEquals("test", passedUserToJWT.getLastName());
+        assertEquals("test@test.com", passedUserToJWT.getEmail());
+        assertEquals("+212600000000", passedUserToJWT.getPhoneNumber());
+        assertEquals("test@123", passedUserToJWT.getPassword());
     }
 }
