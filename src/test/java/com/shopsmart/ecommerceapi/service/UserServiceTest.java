ef542dc428cfb732cc0762d5d5a32768aa7a4d3a
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-26 12:52:19 +0100

    pass new customer registration test in UserService

--- src/test/java/com/shopsmart/ecommerceapi/service/UserServiceTest.java
+++ src/test/java/com/shopsmart/ecommerceapi/service/UserServiceTest.java
@@ -1,22 +1,23 @@
 package com.shopsmart.ecommerceapi.service;
 
+import com.shopsmart.ecommerceapi.model.Role;
 import com.shopsmart.ecommerceapi.model.User;
+import com.shopsmart.ecommerceapi.repository.RoleRepository;
 import com.shopsmart.ecommerceapi.repository.UserRepository;
 import com.shopsmart.ecommerceapi.util.JWTUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,15 +25,16 @@ public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
-
     @Mock
     private JWTUtils jwtUtils;
+    @Mock
+    private RoleRepository roleRepository;
 
     @InjectMocks
     private UserService underTest;
 
     @Test
-    public void givenNewCustomer_whenRegisterCustomer_thenReturnTokenWithCustomerRole() {
+    public void givenNewCustomer_whenRegisterCustomer_thenReturnToken() {
         // Given
         User user = User
                 .builder()
@@ -42,15 +44,24 @@ public class UserServiceTest {
                 .phoneNumber("+212600000000")
                 .password("test@123")
                 .build();
-        given(userRepository.findByEmail(user.getEmail())).willReturn(Optional.empty());
-        given(jwtUtils.generateToken(user)).willReturn("someReturnedToken");
+        User userSpy = spy(user);
+        given(userRepository.findByEmail(userSpy.getEmail())).willReturn(Optional.empty());
+        given(jwtUtils.generateToken(userSpy)).willReturn("someReturnedToken");
+        given(roleRepository.findByName(anyString())).willReturn(Optional.of(new Role(1, "customer")));
 
         // When
-        String returnedValue = underTest.registerCustomer(user);
+        String returnedValue = underTest.registerCustomer(userSpy);
         // Then
         ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
         verify(userRepository).findByEmail(stringCaptor.capture());
-        assertEquals(user.getEmail(), stringCaptor.getValue());
+        assertEquals(userSpy.getEmail(), stringCaptor.getValue());
+
+        verify(roleRepository).findByName("customer");
+
+        ArgumentCaptor<Role> roleCaptor = ArgumentCaptor.forClass(Role.class);
+        verify(userSpy).addRole(roleCaptor.capture());
+        assertEquals("customer", roleCaptor.getValue().getName());
+
         ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
         verify(userRepository).save(userCaptor.capture());
         User passedUserToSave = userCaptor.getValue();
@@ -59,7 +70,7 @@ public class UserServiceTest {
         assertEquals("test@test.com", passedUserToSave.getEmail());
         assertEquals("+212600000000", passedUserToSave.getPhoneNumber());
         assertEquals("test@123", passedUserToSave.getPassword());
-        assertEquals("someReturnedToken", returnedValue);
+
         verify(jwtUtils).generateToken(userCaptor.capture());
         User passedUserToJWT = userCaptor.getValue();
         assertEquals("test", passedUserToJWT.getFirstName());
@@ -67,5 +78,7 @@ public class UserServiceTest {
         assertEquals("test@test.com", passedUserToJWT.getEmail());
         assertEquals("+212600000000", passedUserToJWT.getPhoneNumber());
         assertEquals("test@123", passedUserToJWT.getPassword());
+        assertEquals("someReturnedToken", returnedValue);
+
     }
 }
