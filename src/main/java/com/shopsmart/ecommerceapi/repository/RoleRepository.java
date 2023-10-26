3b14bcf602707090820f1914e6251097811071e2
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-26 12:48:59 +0100

    Add findByName

--- src/main/java/com/shopsmart/ecommerceapi/repository/RoleRepository.java
+++ src/main/java/com/shopsmart/ecommerceapi/repository/RoleRepository.java
@@ -4,6 +4,10 @@ import com.shopsmart.ecommerceapi.model.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
 }
