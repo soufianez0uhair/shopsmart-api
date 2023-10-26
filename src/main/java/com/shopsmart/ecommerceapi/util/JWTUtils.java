ad86d40638e8b91662c954ef89072154e690630c
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-26 12:51:37 +0100

    add token generation method

--- src/main/java/com/shopsmart/ecommerceapi/util/JWTUtils.java
+++ src/main/java/com/shopsmart/ecommerceapi/util/JWTUtils.java
@@ -3,30 +3,34 @@ package com.shopsmart.ecommerceapi.util;
 import com.shopsmart.ecommerceapi.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JWTUtils {
 
     @Value("${JWT_SECRET_KEY}")
-    private static String SECRET_KEY;
+    private String SECRET_KEY;
 
-    private static Key getSignInKey() {
+    private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public static String extractUsername(String token) {
+    public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    private static Claims extractAllClaims(String token) {
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
@@ -35,13 +39,29 @@ public class JWTUtils {
                 .getBody();
     }
 
-    public static <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
         final Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
     }
 
-    public static String generateToken(User user) {
-        return "";
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            User user
+    ) {
+
+        return Jwts
+                .builder()
+                .claim("roles", user.getRoles().toString())
+                .setClaims(extraClaims)
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(User user) {
+        return generateToken(new HashMap<>(), user);
     }
 
 }
