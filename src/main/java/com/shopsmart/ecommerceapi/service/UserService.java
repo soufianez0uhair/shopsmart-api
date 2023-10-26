fcc1111dbe947cb11ce451cc5b27e9a0a869c5f8
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-26 10:51:29 +0100

    add jwtUtils attribute

--- src/main/java/com/shopsmart/ecommerceapi/service/UserService.java
+++ src/main/java/com/shopsmart/ecommerceapi/service/UserService.java
@@ -2,6 +2,7 @@ package com.shopsmart.ecommerceapi.service;
 
 import com.shopsmart.ecommerceapi.model.User;
 import com.shopsmart.ecommerceapi.repository.UserRepository;
+import com.shopsmart.ecommerceapi.util.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +10,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private UserRepository userRepository;
+    private JWTUtils jwtUtils;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(
+            UserRepository userRepository,
+            JWTUtils jwtUtils
+    ) {
         this.userRepository = userRepository;
+        this.jwtUtils = jwtUtils;
     }
 
     public String registerCustomer(User user) {
-        return "";
+        userRepository.findByEmail(user.getEmail());
+        userRepository.save(user);
+        return jwtUtils.generateToken(user);
     }
 }
