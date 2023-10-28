e7828079a4e7f4711760c8da6a9e8181f1acbd65
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-28 18:55:44 +0100

    Add validation sequence order recognizition

--- src/main/java/com/shopsmart/ecommerceapi/controller/UserController.java
+++ src/main/java/com/shopsmart/ecommerceapi/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.shopsmart.ecommerceapi.controller;
 
 import com.shopsmart.ecommerceapi.model.User;
+import com.shopsmart.ecommerceapi.model.ValidationSequenceOrder;
 import com.shopsmart.ecommerceapi.service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    private ResponseEntity<String> registerCustomer(@Valid @RequestBody User user) {
-        return new ResponseEntity<String>(
+    private ResponseEntity<String> registerCustomer(@Validated(ValidationSequenceOrder.class) @RequestBody User user) {
+        return new ResponseEntity<>(
                 userService.registerCustomer(user),
                 HttpStatus.CREATED
         );
