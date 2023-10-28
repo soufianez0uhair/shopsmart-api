97af698968c432c281fb5caee29de02a69263dba
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-28 22:26:09 +0100

    add email validation

--- src/main/java/com/shopsmart/ecommerceapi/model/User.java
+++ src/main/java/com/shopsmart/ecommerceapi/model/User.java
@@ -1,6 +1,7 @@
 package com.shopsmart.ecommerceapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -42,6 +43,7 @@ public class User {
     private String lastName;
 
     @NotBlank(message = "Email is required")
+    @Email(message = "Please use a valid email")
     private String email;
 
     @Column(
