3cb49219f552828cc4d08f23a8001fbd171b179e
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-28 17:26:01 +0100

    Create ApiExceptionHandler and handle @Valid exception

--- src/main/java/com/shopsmart/ecommerceapi/exception/ApiExceptionHandler.java
+++ src/main/java/com/shopsmart/ecommerceapi/exception/ApiExceptionHandler.java
@@ -1,2 +1,37 @@
-package com.shopsmart.ecommerceapi.exception;public class ApiExceptionHandler {
+package com.shopsmart.ecommerceapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiException> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        ApiException apiException = ApiException
+                .builder()
+                .message(errors.get(0))
+                .timestamp(new Date())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .build();
+
+        return new ResponseEntity<ApiException>(
+                apiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
 }
