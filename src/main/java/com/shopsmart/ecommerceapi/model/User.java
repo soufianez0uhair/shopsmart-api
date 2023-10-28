1ec3a4b6cf817dca2dbc7cd65e4fd95d35a7212b
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-29 00:14:15 +0100

    [green]: fix password is required not showing

--- src/main/java/com/shopsmart/ecommerceapi/model/User.java
+++ src/main/java/com/shopsmart/ecommerceapi/model/User.java
@@ -61,8 +61,8 @@ public class User {
             length = 20
     )
     @Size(max = 20, message = "Password must not exceed 20 characters length")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @CreationTimestamp
