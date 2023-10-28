1dff5d5e92b8d12f45183a81ddcdc4fd138a13f2
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-28 14:38:38 +0100

    fix role objects total equality comparison

--- src/main/java/com/shopsmart/ecommerceapi/model/Role.java
+++ src/main/java/com/shopsmart/ecommerceapi/model/Role.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "roles")
 @Data
@@ -19,4 +21,17 @@ public class Role {
     private Integer id;
 
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
