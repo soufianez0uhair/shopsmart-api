231844b12c93f6460d1c37a1c1a1f929dcc4d287
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-28 17:25:02 +0100

    Create api exception model

--- src/main/java/com/shopsmart/ecommerceapi/exception/ApiException.java
+++ src/main/java/com/shopsmart/ecommerceapi/exception/ApiException.java
@@ -1,13 +1,19 @@
 package com.shopsmart.ecommerceapi.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
+@AllArgsConstructor
+@Builder
 @Data
 public class ApiException {
 
     private String message;
-    private HttpStatus status;
-
+    private HttpStatus httpStatus;
+    private Date timestamp;
 
 }
